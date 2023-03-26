a1d0f40e45b9e6944f072b945b300a06fd79b557
davidmp <mamanipari@gmail.com>
2023-03-26 05:10:21 -0500

    implementacion en categoria

---
+++ SysVentasGUIG2/src/main/java/pe/edu/upeu/app/dao/CategoriaDao.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.app.dao;
+
+/**
+ *
+ * @author EP-Ing_Sist.-CALIDAD
+ */
+public class CategoriaDao {
+    
+}
